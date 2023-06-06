4ba01d6dc43f4e96a63d3001faf37e08104ec65b
reXatrom <ajstylejoshua2837@gmail.com>
2023-06-06 14:19:33 +0000

    made an adjustment.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,21 @@ the number of subscribers (not active users, total subscribers)
 for a given subreddit. If an invalid subreddit is given, 
 the function should return 0."""
 import requests
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    '''
+        returns the number of subscribers for a given subreddit
+    '''
+    user = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
+    try:
+        return url.get('data').get('subscribers')
+    except Exception:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
